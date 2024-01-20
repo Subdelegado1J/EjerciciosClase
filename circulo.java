fe99f81e4ee48b8ab8e638aff1db7d909119a851
Subdelegado1J <153597298+Subdelegado1J@users.noreply.github.com>
2024-01-20 20:15:14 +0100

    Create circulo.java

---
+++ circulo.java
@@ -0,0 +1,21 @@
+public class circulo {
+
+    public double radio;
+
+    public circulo (double radio) {
+
+    if (radio < 0){
+       this.radio = 0;
+    } else {
+        this.radio = radio;
+        }
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public double getArea() {
+        return radio * radio * Math.PI;
+    }
+}
